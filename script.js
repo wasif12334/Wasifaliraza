43f246d6561961b557214e6f724ed6ab59aafd0e
wasif12334 <kashifaliraza95@gmail.com>
2024-08-08 20:31:57 +0500

    Added Responsiveness in nav bar

---
+++ script.js
@@ -0,0 +1,9 @@
+//Nav-Responsiveness button 
+let navlinks=document.getElementsByClassName("links");
+function showmenu(){
+   navlinks.style.right="0px";
+}
+function hidemenu(){
+   navlinks.style.right="-200px";
+}
+//---------//
