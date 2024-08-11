82a8e201a81bba6c3ccf753f6d9f046751040b1e
wasif12334 <kashifaliraza95@gmail.com>
2024-08-11 15:50:11 +0500

    Added about+skills section and some responsivenes in it

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function hidemenu(){
    navlinks.style.right="-200px";
 }
 //---------//
+//Marque//
+$(function() {
+   $('marquee').mouseover(function() {
+       $(this).attr('scrollamount',0);
+   }).mouseout(function() {
+        $(this).attr('scrollamount',5);
+   });
+})
+//--------//
